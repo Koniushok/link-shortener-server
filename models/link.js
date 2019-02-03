651ce4cd768874553d2ae9addb6945f6637995e1
Valera <Koniushok@users.noreply.github.com>
2019-02-03 04:37:58 -0900

    add link model

---
+++ models/link.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const linkSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    maxlength: 100,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  shortLink: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  passage: {
+    type: Number,
+    default: 0,
+  },
+  tags: {
+    type: [String],
+    default: [],
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+function validateLink(link) {
+  const schema = {
+    title: Joi.string()
+      .max(100)
+      .required(),
+    url: Joi.string()
+      .uri({ scheme: ['http', 'https'] })
+      .required(),
+    description: Joi.string().required(),
+    tags: Joi.array(),
+  };
+
+  return Joi.validate(link, schema);
+}
+const linkModel = mongoose.model('Link', linkSchema);
+
+exports.Link = linkModel;
+exports.validate = validateLink;
