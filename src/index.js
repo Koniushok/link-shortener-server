7a5f35ae4f9c540a2fa8f6e655b74e5712f94335
Valera <Koniushok@users.noreply.github.com>
2019-02-04 02:32:33 -0900

    connect routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import mongoose from 'mongoose';
 import config from 'config';
+import express from 'express';
+import cors from 'cors';
+import auth from './routes/auth';
+import user from './routes/user';
+import link from './routes/link';
+
+const app = express();
 
 mongoose
   .connect(config.get('database'))
   .then(() => console.log('Connected to MongoDB'))
   .catch(() => console.error('Could not connect to MongoDB'));
+
+app.use(cors());
+app.use(express.json());
+app.use('/auth', auth);
+app.use('/user', user);
+app.use('/link', link);
+
+const port = process.env.PORT || config.get('port');
+app.listen(port, () => console.log(`Listening (port: ${port})`));
