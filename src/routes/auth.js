645511d5c6a98a126efce6073a162bb7c2552168
Valera <Koniushok@users.noreply.github.com>
2019-02-04 02:18:21 -0900

    add auth route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,16 +1,16 @@
 // @flow
-import express from 'express';
-import { userModel, validateUser } from '../models/user';
+import express, { type $Response, type $Request } from 'express';
+import { UserModel, validateUser } from '../models/user';
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {
+router.post('/', async (req: $Request, res: $Response) => {
   const { error } = validateUser(req.body);
   if (error) {
     return res.status(400).send(error.details[0].message);
   }
 
-  const user = await userModel.findOne({ loginName: req.body.loginName });
+  const user = await UserModel.findOne({ loginName: req.body.loginName });
   if (!user) {
     return res.status(400).send('Invalid login or password.');
   }
