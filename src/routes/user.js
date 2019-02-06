a9fb33d4d21eed03462f38e89b862eb2926c7884
Valera <Koniushok@users.noreply.github.com>
2019-02-05 21:38:50 -0900

    fix user route

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,15 +6,21 @@ const router = express.Router();
 
 router.post('/', async (req: $Request, res: $Response) => {
   const { error } = validateUser(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
 
   let user = await UserModel.findOne({ loginName: req.body.loginName });
-  if (user) return res.status(400).send('Such login already exists');
+  if (user) {
+    return res.status(400).send('Such login already exists');
+  }
 
   user = new UserModel(req.body);
-
   await user.save();
-  res.send('Successfully registered');
+
+  delete user.__v;
+  delete user.password;
+  res.send(user);
 });
 
 export default router;
