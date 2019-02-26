6e7a59b5e864bb2b84869e5b79894746d805ed2f
Valera <Koniushok@users.noreply.github.com>
2019-02-26 03:00:55 -0900

    fix global catch

--- src/middleware/error.js
+++ src/middleware/error.js
@@ -1,9 +1,14 @@
 // @flow
-import { type $Response, type $Request } from 'express';
+import { type $Response, type $Request, type NextFunction } from 'express';
 import logger from '../logger';
 
-function error(err: Error, req: $Request, res: $Response) {
-  logger.error(err.message, err);
-  res.status(500).send('Server error.');
+function error(err: Error, req: $Request, res: $Response, next: NextFunction) {
+  if (error) {
+    logger.error(err.message, err);
+    res.status(500).send('Server error.');
+    throw Promise.reject(error);
+  }
+
+  return next();
 }
 export default error;
